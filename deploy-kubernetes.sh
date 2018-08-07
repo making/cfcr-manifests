77ca51c9be1b9511237d576e3b73158a51d2e0a1
Toshiaki Maki <tmaki@pivotal.io>
2018-08-07 08:56:12 +0000

    update

--- deploy-kubernetes.sh
+++ deploy-kubernetes.sh
@@ -13,6 +13,9 @@ bosh deploy -d cfcr kubo-deployment/manifests/cfcr.yml \
     -o ops-files/kubernetes-standard-disk.yml \
     -o ops-files/kubernetes-persistent-disk-type.yml \
     -o ops-files/kubernetes-ingress-lb.yml \
+    -o ops-files/kubernetes-add-syslog.yml \
+    -o ops-files/kubernetes-add-syslog-tls.yml \
+    -o ops-files/kubernetes-log-level.yml \
     --var-file addons-spec=<(for f in `ls specs/*.yml`;do cat $f;echo;echo "---";done) \
     -v kubernetes_cluster_tag=${kubernetes_cluster_tag} \
     -v kubernetes_master_host=${master_lb_ip_address} \
@@ -24,4 +27,11 @@ bosh deploy -d cfcr kubo-deployment/manifests/cfcr.yml \
   value: ((oidc_ca))
 EOF) \
     --var-file oidc_ca=acm.ca \
+    -v syslog_address=${SYSLOG_ADDRESS} \
+    -v syslog_port=${SYSLOG_PORT} \
+    -v syslog_transport=tcp \
+    --var-file syslog_ca_cert=logstash.ca \
+    -l <(cat <<EOF
+syslog_permitted_peer: "${SYSLOG_PERMITTED_PEER}"
+EOF) \
     --no-redact

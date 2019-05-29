c1a1fbdc99928962569fa7e67df685d4bb03664a
Toshiaki Maki <tmaki@pivotal.io>
2019-05-30 01:40:39 +0900

    Add ldap lb

---
+++ terraform/nlb.tf
@@ -0,0 +1,59 @@
+resource "aws_security_group" "ldaps" {
+  name        = "${var.prefix}-bosh-ldaps"
+  description = "LDAPS Security Group"
+  vpc_id      = "${aws_vpc.vpc.id}"
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 636
+    to_port     = 636
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+}
+
+resource "aws_lb" "bosh" {
+  name                             = "${var.prefix}-bosh-lb"
+  subnets                          = ["${aws_subnet.public.*.id}"]
+  load_balancer_type               = "network"
+  enable_cross_zone_load_balancing = true
+}
+
+resource "aws_lb_listener" "bosh_ldaps" {
+  load_balancer_arn = "${aws_lb.bosh.arn}"
+  port              = 636
+  protocol          = "TLS"
+  ssl_policy        = "ELBSecurityPolicy-2015-05"
+  certificate_arn   = "${var.ssl_cert_arn}"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.bosh_ldaps.arn}"
+  }
+}
+
+resource "aws_lb_target_group" "bosh_ldaps" {
+  name     = "${var.prefix}-bosh-ldaps"
+  port     = 636
+  protocol = "TLS"
+  vpc_id   = "${aws_vpc.vpc.id}"
+
+  health_check {
+    protocol = "TCP"
+  }
+}
+
+resource "aws_route53_record" "ldap" {
+  zone_id = "Z19IP7K49DB2RN"
+  name    = "ldap.ik.am"
+  type    = "CNAME"
+  ttl     = 60
+
+  records = ["${aws_lb.bosh.dns_name}"]
+}
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,5 @@
 provider "aws" {
+    version = "1.60.0"
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
     region = "${var.region}"

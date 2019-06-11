b2bd18b442f99b7f4fc224fcba1cb54e1d53b283
Toshiaki Maki <tmaki@pivotal.io>
2019-06-11 14:59:15 +0900

    update nlb

--- terraform/nlb.tf
+++ terraform/nlb.tf
@@ -1,3 +1,30 @@
+resource "aws_security_group" "master" {
+  name        = "${var.prefix}-bosh-master"
+  description = "Master Security Group"
+  vpc_id      = "${aws_vpc.vpc.id}"
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+  }
+  
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 8443
+    to_port     = 8443
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+}
+
 resource "aws_security_group" "ldaps" {
   name        = "${var.prefix}-bosh-ldaps"
   description = "LDAPS Security Group"
@@ -25,6 +52,19 @@ resource "aws_lb" "bosh" {
   enable_cross_zone_load_balancing = true
 }
 
+resource "aws_lb_listener" "bosh_master" {
+  load_balancer_arn = "${aws_lb.bosh.arn}"
+  port              = 443
+  protocol          = "TLS"
+  ssl_policy        = "ELBSecurityPolicy-2015-05"
+  certificate_arn   = "${var.ssl_cert_arn}"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.bosh_master.arn}"
+  }
+}
+
 resource "aws_lb_listener" "bosh_ldaps" {
   load_balancer_arn = "${aws_lb.bosh.arn}"
   port              = 636
@@ -35,6 +75,17 @@ resource "aws_lb_listener" "bosh_ldaps" {
   default_action {
     type             = "forward"
     target_group_arn = "${aws_lb_target_group.bosh_ldaps.arn}"
+  }
+}
+
+resource "aws_lb_target_group" "bosh_master" {
+  name     = "${var.prefix}-bosh-master"
+  port     = 8443
+  protocol = "TLS"
+  vpc_id   = "${aws_vpc.vpc.id}"
+
+  health_check {
+    protocol = "TCP"
   }
 }
 
@@ -52,6 +103,15 @@ resource "aws_lb_target_group" "bosh_ldaps" {
 resource "aws_route53_record" "ldap" {
   zone_id = "Z19IP7K49DB2RN"
   name    = "ldap.ik.am"
+  type    = "CNAME"
+  ttl     = 60
+
+  records = ["${aws_lb.bosh.dns_name}"]
+}
+
+resource "aws_route53_record" "master" {
+  zone_id = "Z1IYLCI9Z4L71D"
+  name    = "k8s.bosh.tokyo"
   type    = "CNAME"
   ttl     = 60
 

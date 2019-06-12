2ec05f7a569bbbdb72cfa9803d9ad9ffaf542078
Toshiaki Maki <tmaki@pivotal.io>
2019-06-12 16:42:16 +0900

    update

--- terraform/nlb.tf
+++ terraform/nlb.tf
@@ -45,6 +45,33 @@ resource "aws_security_group" "ldaps" {
   }
 }
 
+resource "aws_security_group" "prometheus_proxy" {
+  name        = "${var.prefix}-prometheus-proxy"
+  description = "Prometheus Proxy Security Group"
+  vpc_id      = "${aws_vpc.vpc.id}"
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 7001
+    to_port     = 7001
+  }
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 32754
+    to_port     = 32754
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+}
+
 resource "aws_lb" "bosh" {
   name                             = "${var.prefix}-bosh-lb"
   subnets                          = ["${aws_subnet.public.*.id}"]
@@ -78,6 +105,17 @@ resource "aws_lb_listener" "bosh_ldaps" {
   }
 }
 
+resource "aws_lb_listener" "prometheus_proxy" {
+  load_balancer_arn = "${aws_lb.bosh.arn}"
+  port              = 7001
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.prometheus_proxy.arn}"
+  }
+}
+
 resource "aws_lb_target_group" "bosh_master" {
   name     = "${var.prefix}-bosh-master"
   port     = 8443
@@ -100,6 +138,17 @@ resource "aws_lb_target_group" "bosh_ldaps" {
   }
 }
 
+resource "aws_lb_target_group" "prometheus_proxy" {
+  name     = "${var.prefix}-prometheus-proxy"
+  port     = 32754
+  protocol = "TCP"
+  vpc_id   = "${aws_vpc.vpc.id}"
+
+  health_check {
+    protocol = "TCP"
+  }
+}
+
 resource "aws_route53_record" "ldap" {
   zone_id = "Z19IP7K49DB2RN"
   name    = "ldap.ik.am"
@@ -112,6 +161,15 @@ resource "aws_route53_record" "ldap" {
 resource "aws_route53_record" "master" {
   zone_id = "Z1IYLCI9Z4L71D"
   name    = "k8s.bosh.tokyo"
+  type    = "CNAME"
+  ttl     = 60
+
+  records = ["${aws_lb.bosh.dns_name}"]
+}
+
+resource "aws_route53_record" "prometheus_proxy" {
+  zone_id = "Z1IYLCI9Z4L71D"
+  name    = "prometheus-proxy.dev.bosh.tokyo"
   type    = "CNAME"
   ttl     = 60
 

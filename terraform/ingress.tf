f18e2ab2cd5bc59798f93ebb2965ec21736cc1c9
Toshiaki Maki <tmaki@pivotal.io>
2019-07-15 15:55:13 +0000

    update ingress

--- terraform/ingress.tf
+++ terraform/ingress.tf
@@ -63,6 +63,22 @@ resource "aws_lb_listener_rule" "cfcr_ingress_note" {
   }
 }
 
+resource "aws_lb_listener_rule" "cfcr_ingress_build" {
+  listener_arn = "${aws_lb_listener.front_end.arn}"
+  priority     = 29
+  count        = "${var.use_alb ? 1 : 0}"
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.cfcr_ingress_https.arn}"
+  }
+
+  condition {
+    field  = "host-header"
+    values = ["build.dev.bosh.tokyo"]
+  }
+}
+
 resource "aws_route53_record" "grafana" {
   zone_id = "Z1IYLCI9Z4L71D"
   name    = "grafana.k8s.bosh.tokyo"
@@ -84,6 +100,15 @@ resource "aws_route53_record" "moneygr" {
 resource "aws_route53_record" "note" {
   zone_id = "Z19IP7K49DB2RN"
   name    = "note.ik.am"
+  type    = "CNAME"
+  ttl     = 60
+
+  records = ["${aws_lb.front_end.dns_name}"]
+}
+
+resource "aws_route53_record" "build" {
+  zone_id = "Z1IYLCI9Z4L71D"
+  name    = "build.dev.bosh.tokyo"
   type    = "CNAME"
   ttl     = 60
 
